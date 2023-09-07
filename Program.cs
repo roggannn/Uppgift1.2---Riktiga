94f3767c94eac3a3891fd10c28dac470b9b51daa
roggannn <144336997+roggannn@users.noreply.github.com>
2023-09-07 13:21:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("A FATAL EXCEPTION HAS OCCURED ERROR CODE:12312222398192");
+            Console.WriteLine("REBOOTING COMPUTER IN 10 SECONDS");
+            Console.ReadKey();
+
+        }
+
+    }
+
+
+
+
+}
